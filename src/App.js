013575a613934878f46c45c66a396e77a46f0625
adzinets <adzinetskatsiaryna@gmail.com>
2020-04-13 10:57:14 +0300

    add quliti, metod map

--- src/App.js
+++ src/App.js
@@ -6,11 +6,19 @@ import Messege from "./components/messege/messege"
 import Nik from "./components/nik/nik";
 
 const App = () => {
+    let qualities = [
+        {id:0, title: 'communist', type: false},
+        {id:1, title: 'liberall', type: true},
+        {id:2, title: 'monarchist', type: false},
+        {id:3, title: 'anarchist', type: false},
+        {id:4, title: 'nationalist', type: false},
+    ];
+
   return (
       <div className='app-wrapper'>
        <Nik/>
       <Avatar/>
-      <Messege/>
+      <Messege quality={qualities}/>
       </div>
 )
 }
--- src/components/messege/messege.jsx
+++ src/components/messege/messege.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import classes from './messege.module.css'
 
 
-const Message = () => {
+const Message = (props) => {
+
+        let spanQualities = props.quality.map(q => {
+            return <span key={q.id} className={q.type ? classes.activeClass : ''}> {q.title + ' / '}</span>
+        });
+
     return (
         <div className={classes.message}>
             <p className={classes.subject}>Одинец Екатерина</p>
             <p className={classes.content}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit olore eu fugiat nulla pariatur.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit olore eu fugiat nulla pariatur. lorem jdfhakjsfn jfdskhlwjdskv vdjasbvkja.bg.aefd dnsmabvajk.dfakj kdjsbgvkajsdbvaedflkjbekaerj.bv fjkbhkjaseaq fbgdskjlfb
             </p>
+            <div className={classes.quality}>{spanQualities}</div>
             <p className={classes.time}>{new Date().toLocaleTimeString( 'ru-RU', {hour: '2-digit', minute: '2-digit'})}
             </p>
         </div>
